39b1dfea7a06bea4d4a2991cce123f163d0ae84b
skymandr <skymandr@fripost.org>
2016-01-14 11:08:08 +0100

    Wrote a comment on squareness of waves
    Wrote a comment on squareness of waves in sounddemo.c

--- src/sounddemo.c
+++ src/sounddemo.c
@@ -5,5 +5,7 @@
 //     $ gcc sounddemo.c -o sounddemo && sounddemo | aplay
 // Public domain. ABSOLUTELY NO WARRANTY!
 
+// Adding &64 at the end makes the waveform more square, which gives
+// nicer overtones and a more pleasant listening experience:
 // main(t){for(t=0;;t++)putchar((t*(((t>>12)|(t>>8))&(63&(t>>4))))&64);}
 main(t){for(t=0;;t++)putchar((t*(42&t>>10))&64);}
